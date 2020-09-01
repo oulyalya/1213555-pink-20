adb248b71f0aa744a5df0c731a2ab0b89ea0bfba
Gelezka20160321\oulyalya <olya.debelaya@gmail.com>
2020-09-01 17:47:33 +0300

    index

--- source/js/script.js
+++ source/js/script.js
@@ -16,3 +16,30 @@
   hideMenu();
   menuBtn.addEventListener('click', toggleMenu);
 })();
+
+
+// var reviewsLeft = document.querySelector('.slider__btn--left');
+// var reviewsRight = document.querySelector('.slider__btn--right');
+// var slides = document.querySelectorAll('.slider__slide');
+
+
+
+// var showNextSlide = function () {
+  // var currentSlide = slides[0];
+
+  // for (var i = 0; i < slides.length; i++) {
+  //   if (i < slides.length - 1) {
+      // currentSlide.classList.remove('slider__slide--active');
+
+  //     console.log("click")
+  //   } else {
+  //     console.log("stop")
+  //   }
+  // }
+// }
+
+/*reviewsLeft.addEventListener ('click', function () {
+console.log('left');
+})*/
+
+// reviewsRight.addEventListener('click', showNextSlide);
